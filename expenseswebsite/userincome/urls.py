f44643a189b51631c03ede91a5151b1c9326b267
Pablo Cacciagioni <pablo1234caccia@gmail.com>
2025-02-17 20:17:55 -0300

    Day21

--- expenseswebsite/userincome/urls.py
+++ expenseswebsite/userincome/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('add-income', views.add_income, name="add-income"),
     path('edit-income/<int:id>', views.income_edit, name="income-edit"),
     path('income-delete/<int:id>', views.delete_income, name="income-delete"),
-    path('search-income',csrf_exempt(views.search_income), name="search-income")
+    path('search-income',csrf_exempt(views.search_income), name="search-income"),
+    path('income_category_summary', views.income_category_summary, name="income_category_summary"),
+    path('stats', views.stats_view, name="stats_income")
 ]
